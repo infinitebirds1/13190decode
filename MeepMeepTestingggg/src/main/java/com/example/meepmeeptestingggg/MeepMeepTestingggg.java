cdee700f6be571da50d6370af9259d3793da77d1
Your Name <you@example.com>
2025-11-21 16:59:14 -0800

    meepmeep 11/21 + amongus

--- MeepMeepTestingggg/src/main/java/com/example/meepmeeptestingggg/MeepMeepTestingggg.java
+++ MeepMeepTestingggg/src/main/java/com/example/meepmeeptestingggg/MeepMeepTestingggg.java
@@ -1,6 +1,8 @@
 package com.example.meepmeeptestingggg;
 
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.TrajectoryBuilder;
+import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
@@ -14,15 +16,26 @@ public class MeepMeepTestingggg {
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(0, 0, Math.toRadians(90)))
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-40 , 0, Math.toRadians(90)))
 
-                .lineToY(30)
+
+                .splineTo(new Vector2d(0, 25), Math.toRadians(0))
+                .splineTo(new Vector2d(35, 0), Math.toRadians(0))
+                .splineTo(new Vector2d(0, -10), Math.toRadians(0))
+                .build());
+
+
+             /*   .lineToY(30)
                 .turn(Math.toRadians(90))
                 .lineToX(0)
                 .turn(Math.toRadians(90))
                 .lineToY(0)
                 .turn(Math.toRadians(90))
                 .build());
+
+
+
+              */
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_JUICE_DARK)
                 .setDarkMode(true)
